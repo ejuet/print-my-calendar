671739bd9890a0d2cfa6608daeead39c4e60b860
eroeb <97666589+eroeb@users.noreply.github.com>
2023-08-06 18:38:23 +0200

    read files

--- src/App.js
+++ src/App.js
@@ -4,20 +4,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Upload File</h1>
+      <input type="file" accept='ics' multiple onChange={(e) => {
+
+        var files = e.target.files;
+        for (let i = 0; i < files.length; i++) {
+          
+          let fr = new FileReader();
+          fr.onload = function () {
+            console.log(fr.result);
+          }
+          fr.readAsText(files[i]);
+        }
+      }}></input>
     </div>
   );
 }
