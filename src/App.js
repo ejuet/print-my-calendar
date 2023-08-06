e37c1e73bd7179a4018f54ce761b64d427b86ad1
eroeb <97666589+eroeb@users.noreply.github.com>
2023-08-06 19:53:52 +0200

    start to use calendar events, add example .ics files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import * as ICAL from "ical.js" 
+import { testcontent } from './testics';
+import { testcontent2 } from './testics2';
 
 function App() {
+  
+  exampleReadICS(testcontent2)
+
   return (
     <div className="App">
       <h1>Upload File</h1>
@@ -9,10 +15,12 @@ function App() {
 
         var files = e.target.files;
         for (let i = 0; i < files.length; i++) {
-          
+
           let fr = new FileReader();
           fr.onload = function () {
             console.log(fr.result);
+            console.log(typeof fr.result);
+            exampleReadICS(fr.result);
           }
           fr.readAsText(files[i]);
         }
@@ -22,3 +30,19 @@ function App() {
 }
 
 export default App;
+
+
+function exampleReadICS(textcontent) {
+  var data = ICAL.parse(textcontent);
+  console.log(data);
+  var vcal = new ICAL.Component(data);
+  var events = vcal.getAllSubcomponents("vevent");
+  for (let j = 0; j < events.length; j++) {
+    var ev = new ICAL.Event(events[j]);
+    console.log(ev.summary);
+    console.log(ev.startDate.toJSON());
+    console.log(ev.isRecurring());
+    
+  }
+}
+
