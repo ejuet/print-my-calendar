253d09e8ad6ad4ab8386080f662ec4117b552d96
eroeb <97666589+eroeb@users.noreply.github.com>
2023-10-01 14:01:12 +0200

    routing

--- src/App.js
+++ src/App.js
@@ -5,35 +5,16 @@ import { testcontent } from "./testics";
 import { testcontent2 } from "./testics2";
 import { CalendarList, ListEvents, exampleReadICS } from "./calendar.tsx";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Nav, NavDropdown, Navbar } from "react-bootstrap";
 
 function App() {
   exampleReadICS(testcontent2);
 
   return (
     <div className="App">
-      <MyNavbar />
-      
       <CalendarList />
     </div>
   );
 }
 
-function MyNavbar(){
-  return <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
-  <Container>
-    <Navbar.Brand href="#home">Print Your Calendar</Navbar.Brand>
-    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-    <Navbar.Collapse id="responsive-navbar-nav">
-      <Nav className="me-auto">
-        <Nav.Link >Calendar Download</Nav.Link>
-      </Nav>
-      <Nav>
-        <Nav.Link href="#deets">Info & Credits</Nav.Link>
-      </Nav>
-    </Navbar.Collapse>
-  </Container>
-</Navbar>
-}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,13 +3,76 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  NavLink
+} from "react-router-dom";
 
+import { Container, Nav, NavDropdown, Navbar } from "react-bootstrap";
+import { CalendarList } from './calendar.tsx';
+
+
+/*
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+*/
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <WithNavbar>
+      
+    </WithNavbar>
+  },
+  {
+    path: "calendar",
+    element: <WithNavbar>
+      <CalendarList />
+    </WithNavbar>
+  },
+  {
+    path: "credits",
+    element: <WithNavbar>
+
+    </WithNavbar>
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <div className='App'>
+    <RouterProvider router={router} />
+  </div>
+);
+
+function WithNavbar({ children }) {
+  return <>
+    <MyNavbar />
+    {children}
+  </>
+}
+
+function MyNavbar() {
+  return <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+    <Container>
+      <Navbar.Brand href="">Print Your Calendar</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="me-auto">
+          <NavLink to="/calendar">Calendar Download</NavLink>
+        </Nav>
+        <Nav>
+          <NavLink to="/credits">Info & Credits</NavLink>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
+}
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
