6fe74b76d7319a0a24a8102be112363d2bfc179a
eroeb <97666589+eroeb@users.noreply.github.com>
2026-05-15 14:01:17 +0200

    comments

--- src/calendar/renderers/types.ts
+++ src/calendar/renderers/types.ts
@@ -9,15 +9,27 @@ export type RendererCommonProps = {
 	eventNameReplacements: Record<string, string>;
 };
 
+/**
+ * Props for the react component that renders the preview of a calendar renderer.
+ */
 export type RendererPreviewProps<TSettings = unknown> = RendererCommonProps & {
 	settings: TSettings;
 };
 
+/**
+ * Props for the react component that renders the display settings of a calendar renderer.
+ * It includes the current settings and a function to update the settings.
+ */
 export type RendererSettingsProps<TSettings = unknown> = {
 	settings: TSettings;
 	setSettings: React.Dispatch<React.SetStateAction<TSettings>>;
 };
 
+/**
+ * Represents a calendar renderer, which is responsible for rendering the calendar
+ * in a specific format (e.g., month table, week view, etc.)
+ * and providing the necessary components for previewing and configuring the renderer.
+ */
 export interface CalendarRenderer<TSettings = unknown> {
 	id: string;
 	name: string;
